3432097f8ca69dcaa8846766822c221f02f31dfc
Kostadin Seymenski <seimenski@icloud.com>
2021-04-13 16:46:18 +0300

    Homepage and Single Product Component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
