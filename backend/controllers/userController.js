11c9fb2f218c0d1d6dc1ddfd8b6cfc465418b565
Kostadin Seymenski <seimenski@icloud.com>
2021-04-15 00:55:39 +0300

    Get user on profile page. Update profile functionality implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,6 +37,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -66,4 +91,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+export { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,9 +11,11 @@ const protect = asyncHandler(async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
+
       next();
     } catch (error) {
       console.error(error);
@@ -26,8 +28,6 @@ const protect = asyncHandler(async (req, res, next) => {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
-
-  next();
 });
 
 export { protect };
