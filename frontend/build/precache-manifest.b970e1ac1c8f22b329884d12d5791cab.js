4944c816493300fbf9d827f98f1ca06e3819310e
Kostadin Seymenski <seimenski@icloud.com>
2021-04-17 13:16:16 +0300

    project build made

---
+++ frontend/build/precache-manifest.b970e1ac1c8f22b329884d12d5791cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7882159af3d06815e8ccd17eed885d57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ec986a13902dbd1114",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ec986a13902dbd1114",
+    "url": "/static/js/main.8caa8264.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
